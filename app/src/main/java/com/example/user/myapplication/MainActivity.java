45b154362d36735176913dbc566603687e43d41c
douzmee <do.u.z.mee@gmail.com>
2016-07-30 15:02:53 +0700

    Finish 30July

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } //main Method สิ่งที่อยู่ใน method เรียก statement ต้องมี ; อยู่เสมอ
+
+    public void clickAboutMe(View view) {
+
     }
-}
+
+} //Main Class นี่คือ คลาสหลัก
