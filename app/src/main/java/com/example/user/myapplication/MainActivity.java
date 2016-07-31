2d6c0c296cbdf5952dbaa3f1271ffb6d2b75fc2c
douzmee <do.u.z.mee@gmail.com>
2016-07-31 10:58:46 +0700

    INTENT

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private String urlYoutubeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
     } //main Method สิ่งที่อยู่ใน method เรียก statement ต้องมี ; อยู่เสมอ
 
     public void clickAboutMe(View view) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.phonton2);
+        mediaPlayer.start();
+
+        //Intent to WebView
+        urlYoutubeString = "https://youtu.be/5lGoQhFb4NM";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
+
 
     }
 
